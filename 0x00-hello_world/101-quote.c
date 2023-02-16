53491c8a1521acdf19e48c7dc0976631884f4338
dancan-agweli <agwelidancan32@gmail.com>
2023-02-16 13:52:31 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - entry
- * Return: 1 (success)
+ * main - Entry
+ * Return: 1
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
