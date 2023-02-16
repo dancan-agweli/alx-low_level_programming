d4d3a54b78ebe92b049fdcaad59474c08434d62b
dancan-agweli <agwelidancan32@gmail.com>
2023-02-16 13:37:47 -0800

    101

---
+++ 0x00-hello_world/01-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - my value of last quiz
+ * Return: 1
+ */
+int main(void)
+{
+	document(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+	return (1);
+}
