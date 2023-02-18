2b6511220f46271037a5fb5be11751551b3d95f3
dancan-agweli <agwelidancan32@gmail.com>
2023-02-18 12:41:38 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: away 0
+ */
+int main(void)
+{
+	char lowerc;
+
+	for (lowerc = 'a'; lowerc <= 'z'; lowerc++)
+		putchar (lowerc);
+			putchar ('\n');
+			return (0);
+}
+
