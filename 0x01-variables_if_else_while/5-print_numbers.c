bd81a4daf5f6004f61e760cc400c9ce25a259eba
dancan-agweli <agwelidancan32@gmail.com>
2023-02-18 13:17:34 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print a number
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("this numbers are %d\n", a);
+	return (0);
+}
