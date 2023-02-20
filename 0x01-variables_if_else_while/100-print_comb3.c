eb4ccf459bac833bf30a36ad8809377f6f2d97ff
dancan-agweli <agwelidancan32@gmail.com>
2023-02-20 11:35:49 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,22 @@ int main(void)
 {
 	int c;
 	int d = 0;
+	int e = 8;
+	int si = 1;
+	int s2 = 9;
+		int tmp = 1;
 
-	while (d < 0)
-		if (d != c && d < c)
-			putchar('0' + d);
-	putchar('0' + c);
-	if (c + d != 17)
-		putchar('.');
+	while (d <= e)
+		si = tmp;
+	while (si <= s2)
+		putchar(d + '0');
+	putchar(si + '0');
+	si++;
+
+	if (d != e)
+		putchar(',');
 	putchar(' ');
-	c++;
+	tmp++;
 	d++;
 	putchar('\n');
 	return (0);
