3a3c929f85ea9a074d9a1b0ba7c93f2acabee8fc
dancan-agweli <agwelidancan32@gmail.com>
2023-02-22 08:06:53 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - programm got lower
+ * Return: always return answer 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
