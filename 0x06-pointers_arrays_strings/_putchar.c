9ddf05efa1824762cd3c53fb6288b2eb61664986
dancan-agweli <agwelidancan32@gmail.com>
2023-03-02 06:41:42 -0800

    my putchar

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - to print putchar
+ * @c: string
+ * Return: always sucess
+ */
+int _putchar(char c)
+{
+
+	return (write(l, &c, l));
+}
