6f2c39d7562b262b40c8017b0fb34e4219660f7d
dancan-agweli <agwelidancan32@gmail.com>
2023-04-03 12:19:58 -0700

    stringly linked

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,20 +2,14 @@
 #define "LISTS_H"
 
 #include <stdlib.h>
-
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*
+*/
 typedef struct listint_s
 {
 	int n;
