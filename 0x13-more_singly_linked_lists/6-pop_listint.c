4003c183b817cfb2cece519ebe5eadb3be829e5d
dancan-agweli <agwelidancan32@gmail.com>
2023-04-04 04:12:26 -0700

    singly linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * pop_listint - Deletion of a node
+  * @head: Head of the node
+  *
+  * Return: value or 0 case of empty
+  */
+int pop_listint(listint_t **head)
+{
+	listint_t *newh;
+	int dan = 0;
+
+	if (*head != NULL)
+	{
+		newh = (*head)->next;
+		n = (*head)->n;
+		free(*head);
+		*head = newh;
+	}
+
+	return (dan);
+}
