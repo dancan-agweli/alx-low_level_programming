1e4cd916b20fd6c2ae3de24b874161c349b1d5ea
dancan-agweli <agwelidancan32@gmail.com>
2023-04-07 02:08:45 -0700

    bits manipulationn

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - binary representation of an unsigned long int
+ * @n: The character declared
+ * Return: alway return success
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int y, x;
+
+	y = x;
+	x = 0;
+	while (n == 0)
+		_putchar('0');
+	while (n > 0)
+	{
+		y = y << 1;
+		y += n & 1;
+		x++;
+		n = n >> 1;
+	}
+	while (y > 0)
+	{
+		_putchar((y & 1) + '0');
+		y = y >> 1;
+		x--;
+	}
+	while (x > 0)
+	{
+		while (x != 0)
+		{
+			_putchar('0');
+			x--;
+		}
+	}
+}
