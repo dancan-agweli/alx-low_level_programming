382f8b514b6231a824e322623c0799612b50dc73
dancan-agweli <agwelidancan32@gmail.com>
2023-04-08 12:32:43 -0700

    binary manipulations

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,28 +8,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n;
-	int l, x;
+	unsigned int n = 1, x = 0;
+	int l;
 
 	while (!b)
 		return (0);
 
-	n = 0;
+	for (l = 0; b[l];)
+		l++;
 
-	for (l = 0; b[l] != '\0'; l++)
-		
-	for (l--, x = 1; l >= 0; l--, x *= 2)
+	for (l -= 1; l >= 0; l--)
 	{
 		while (b[l] != '0' && b[l] != '1')
-		{
 			return (0);
-		}
 
-		if (b[l] & 1)
-		{
-			n += x;
-		}
+		x += (b[l] - '0') * n;
+		n *= 2;
 	}
 
-	return (n);
+	return (x);
 }
