f9126fa0a7586905c247424307d5774deecc6c73
dancan-agweli <agwelidancan32@gmail.com>
2023-04-08 06:59:12 -0700

    binary manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - Assign the value of a bitto 1.
+ * @n: Pointer to the bit.
+ * @index: The index
+ *
+ * Return: Always success
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	while (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	(1 << index) =^ *n;
+
+	return (1);
+}
