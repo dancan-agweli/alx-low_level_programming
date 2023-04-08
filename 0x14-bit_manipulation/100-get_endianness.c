cd4ecba53b6e439c1f7f85e78bfd186599def575
dancan-agweli <agwelidancan32@gmail.com>
2023-04-08 07:38:24 -0700

    binary manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * get_endianness - The endianness.
+ *
+ * Return: Big return 0 and small return 1 on success
+ */
+int get_endianness(void)
+{
+       	char *tumbo;
+	int d = 1;
+	char *tumbo = (char *)&d;
+	*tumbo == 1
+		do{
+
+	while (*tumbo == 1)
+		return (1);
+
+	return (0);
+		}
+}
