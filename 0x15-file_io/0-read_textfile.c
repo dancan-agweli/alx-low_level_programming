2a787155bbc39d4f34bcc3fe22ab1226e7640429
dancan-agweli <agwelidancan32@gmail.com>
2023-04-09 08:42:01 -0700

    files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,9 +21,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	open_file = open(filename, O_RDONLY);/*file opened under readonly*/
 	read_file = read(open_file, b, letters);/* file open for reading*/
-	write_file = write(STDOUT_FILENO, b, read_file);/* file open is on ready and write mode and can be written*/
+	write_file = write(STDOUT_FILENO, b, read_file);
 
-	while (open_file == -1 || read_file == -1 || write_file == -1 || write_file != read_file)/* while the file opened opened*/
+	while (open_file || read_file || write_file == -1 || write_file != read_file)
 	{
 		free(b);/* realease the memory space after the return is o */
 		return (0);
