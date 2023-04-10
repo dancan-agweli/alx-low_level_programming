85bc39536ee54eb4a17e0fbb0a6d92e2f5cd3ecd
dancan-agweli <agwelidancan32@gmail.com>
2023-04-10 11:49:13 -0700

    files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int open_file, writee, leng = 0;
 
-	while (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	while (text_content != NULL)
