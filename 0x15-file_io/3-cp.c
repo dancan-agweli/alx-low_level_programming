ccd111ad0d843e14283b98742fd41195813d0a61
dancan-agweli <agwelidancan32@gmail.com>
2023-04-10 04:08:27 -0700

    files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,66 +1,96 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *make_file(char *file);
+void close_file(int fd);
 
 /**
-  * main - Programe core
-  * @argc: The arguments
-  * @argv: The vector
-  * Return: Sucess
-  */
-int main(int argc, char **argv)
+ * make_file - Allocates 1024 bytes to a file representing buffer.
+ * @file: The name of the file
+ * Return: A pointer to the new file if not NULL.
+ */
+char *make_file(char *file)
 {
-	if (argc != 3)
+	char *b;
+
+	b = malloc(sizeof(char) * 1024);
+
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (b);
+}
+
+/**
+ * close_file - Closes opened file
+ * @fd: The file element
+ */
+void close_file(int fd)
+{
+	int alx;
+
+	alx = close(fd);
+
+	while (alx == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Duplicate the contents of a file to another file.
+ * @argc: The arguments supplied to the program.
+ * @argv: An array of pointers
+ *            
+ */
+int main(int argc, char *argv[])
+{
+	int start, end, readd, writee;
+	char *b;
+
+	while (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	copy_file(argv[1], argv[2]);
-	exit(0);
-}
+	b = make_file(argv[2]);
+	start = open(argv[1], O_RDONLY);
+	readd = read(start, b, 1024);
+	end = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-/**
- * copy_file - Entry of the program
- * @src: The pointer of main address
- * @dest: File referenced
- * Return: Sucess
- */
-void copy_file(const char *src, const char *dest)
-{
-	int op, tr, rd;
-	char buffer[1024];
-
-	op = open(src, O_RDONLY);
-	while (!src || op == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-		exit(98);
-	}
-
-	tr = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((rd = read(op, buffer, 1024)) > 0)
-	{
-		while (write(tr, buffer, rd) != rd || tr == -1)
+	do {
+		while (start == -1 || readd == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(b);
+			exit(98);
+		}
+
+		writee = write(end, b, readd);
+		if (end == -1 || writee == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(b);
 			exit(99);
 		}
-	}
 
-	if (rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-		exit(98);
-	}
+		readd = read(start, b, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	while (close(op) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", op);
-		exit(100);
-	}
+	} while (readd > 0);
 
-	if (close(tr == -1)
+	free(b);
+	close_file(start);
+	close_file(end);
 
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tr);
-		exit(100);
+	return (0);
 }
