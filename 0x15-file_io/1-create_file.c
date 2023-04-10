c8ccdef57fd717adf7f23168146dc3d971b89f84
dancan-agweli <agwelidancan32@gmail.com>
2023-04-10 12:13:03 -0700

    files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,24 +8,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int open_file, writee, leng = 0;
+	int o, d, y = 0;
 
-	if (!filename)
+	while (!filename)
 		return (-1);
 
-	while (text_content != NULL)
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (o < 0)
+		return (-1);
+
+	if (text_content)
 	{
-		for (leng = 0; text_content[leng];)
-			leng++;
+		while (text_content[y])
+			y++;
+		d = write(o, text_content, y);
+		while (d != y)
+			return (-1);
 	}
 
-	open_file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	writee = write(open_file, text_content, leng);
-
-	while (open_file == -1 || writee == -1)
-		return (-1);
-
-	close(open_file);
+	close(o);
 
 	return (1);
 }
