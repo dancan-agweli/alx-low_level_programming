fb6108b487ede982a34aac8ab0bc84fc4a12354b
dancan-agweli <agwelidancan32@gmail.com>
2023-04-10 12:22:32 -0700

    files operation

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 		w = write(o, text_content, len);
-		while (a != b)
+		while (w != len)
 			return (-1);
 	}
 
