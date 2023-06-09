076f38392a7b569726ff55b4826bca1f4041bedc
dancan-agweli <agwelidancan32@gmail.com>
2023-06-09 13:35:02 -0700

    tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the size or arry
+ * Return: always success
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *buff;
+	unsigned long int c;
+
+	buff = malloc(sizeof(hash_table_t));
+	if (buff == NULL)
+		return (NULL);
+	buff->size = size;
+	buff->array = malloc(size * sizeof(hash_node_t *));
+	if (buff->array == NULL)
+	{
+		free(buff);
+		return (NULL);
+	}
+	for (c = 0; c < size; c++)
+		buff->array[i] = NULL;
+	return (buff);
+}
